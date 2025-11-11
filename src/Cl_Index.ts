818382a417480a9826c9e028c21bbc6f5a69ae05
CarlosColmenarez2 <carloscolmenarezjr2@gmail.com>
2025-11-11 09:02:57 -0400

    Mensaje descriptivo sobre los cambios realizados

---
+++ src/Cl_Index.ts
@@ -0,0 +1,11 @@
+import Cl_controlador from "./Cl_controlador.js";
+import Cl_MClinica from "./Cl_MClinica.js";
+import Cl_vClinica from "./Cl_vClinica.js";
+
+export default class Cl_index {
+    constructor() {
+        const modelo = new Cl_MClinica();
+        const vista = new Cl_vClinica();
+        new Cl_controlador(modelo, vista);
+    }
+}
