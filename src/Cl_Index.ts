3db07786d49ef9ac60dc7188c7d004d2c4d5ee46
CarlosColmenarez2 <carloscolmenarezjr2@gmail.com>
2025-11-11 09:29:21 -0400

    Corregir nombre compilado de archivo a Cl_MClinica

--- src/Cl_Index.ts
+++ src/Cl_Index.ts
@@ -2,7 +2,7 @@ import Cl_controlador from "./Cl_controlador.js";
 import Cl_MClinica from "./Cl_MClinica.js";
 import Cl_vClinica from "./Cl_vClinica.js";
 
-export default class Cl_index {
+export default class Cl_Index {
     constructor() {
         const modelo = new Cl_MClinica();
         const vista = new Cl_vClinica();
